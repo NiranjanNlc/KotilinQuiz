838f881421f64c2cd47ceb72d120797024d6b823
niranjannlc <niranjannlc10@gmail.com>
2021-09-18 20:37:12 +0800

    progress bar not being updated

--- app/src/main/java/com/example/quiz/ui/addquestion/CountDownFragment.kt
+++ app/src/main/java/com/example/quiz/ui/addquestion/CountDownFragment.kt
@@ -1,16 +1,20 @@
 package com.example.quiz.ui.addquestion
 
+import android.animation.ObjectAnimator
 import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import com.example.quiz.R
-import com.example.quiz.databinding.FragmentCountDownBinding
-import com.mikhaellopez.circularprogressbar.CircularProgressBar
+import com.example.quiz.databinding.FragmentCountDownBinding 
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,50 +25,58 @@ private const val ARG_PARAM2 = "param2"
 class CountDownFragment : Fragment() {
 
    private lateinit var binding: FragmentCountDownBinding;
+    private lateinit var mProgress:ProgressBar
+    private lateinit var tv:TextView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding= FragmentCountDownBinding.inflate(layoutInflater)
-        return inflater.inflate(R.layout.fragment_count_down, container, false)
-    }
+        mProgress = binding.circularProgressbar
+        tv=binding.tv
+        val  res = getResources();
+        val  drawable = res.getDrawable(R.drawable.circular); 
+        mProgress.apply {
+            setProgress(0);   // Main Progress
+            setSecondaryProgress(100); // Secondary Progress
+            setMax(100); // Maximum Progress
+            setProgressDrawable(drawable);
+        }
+//        ObjectAnimator.ofInt(mProgress, "progress", 0, 100)
+//            .apply {
+//               setDuration(1000)
+//               setInterpolator(DecelerateInterpolator());
+//               start();
+//            }
+
+            return inflater.inflate(R.layout.fragment_count_down, container, false)
+        }
 
     /**
      * Called when the Fragment is visible to the user.  This is generally
      * tied to [Activity.onStart] of the containing
      * Activity's lifecycle.
      */
-    override fun onStart()
-    {
+    override fun onStart() {
         super.onStart()
-
-    }
-
-    /**
-     * Called when the fragment is visible to the user and actively running.
-     * This is generally
-     * tied to [Activity.onResume] of the containing
-     * Activity's lifecycle.
-     */
-    override fun onResume() {
-        super.onResume()
-        startTimer()
-    }
-
-    private fun startTimer() {
-       val countDownTimer = object : CountDownTimer(0, 1000) {
-            //            end of timer
-            override fun onFinish()
-            {
+        requireActivity().runOnUiThread {
+            val countDownTimer = object : CountDownTimer(20000, 1000) {
+                //            end of timer
+                override fun onFinish()
+                {
 
                 }
 
-            override fun onTick(millisUntilFinished: Long) {
+                override fun onTick(millisUntilFinished: Long)
+                {
+                    Log.d(" tickeld ",millisUntilFinished.toString())
+                    mProgress.max=100
+                    mProgress.progress= ((millisUntilFinished/1000).toInt());
+                    tv.text = millisUntilFinished.toString() + "%";
+                }
 
             }
-
-        }.start()
-
+            countDownTimer.start()
         }
-
+    }
 }
